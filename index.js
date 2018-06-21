0ed5296eea46dffa69bfc9efaee2e92febb58e5d
Essence Alexander <upperlefty@zoho.eu>
2018-06-20 23:46:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function bubblesort(array) {
+    for(let i = 0; i < array.length; i++) {
+        for(let j = 0; j < array.length - 1; j++) {
+            if(array[j] > array[j + 1]) {
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+bubbleSort([7,5,2,4,3,9]); //[2, 3, 4, 5, 7, 9]
+bubbleSort([9,7,5,4,3,1]); //[1, 3, 4, 5, 7, 9]
+bubbleSort([1,2,3,4,5,6]); //[1, 2, 3, 4, 5, 6]
+
+
+
+
 function bubbleSort(arr){
    var len = arr.length;
   for (var i = len-1; i>=0; i--){
@@ -11,6 +29,3 @@ function bubbleSort(arr){
    }
    return arr;
 }
-bubbleSort([7,5,2,4,3,9]); //[2, 3, 4, 5, 7, 9]
-bubbleSort([9,7,5,4,3,1]); //[1, 3, 4, 5, 7, 9]
-bubbleSort([1,2,3,4,5,6]); //[1, 2, 3, 4, 5, 6]
