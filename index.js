f99408f7b2920748a97d90f219a63bfe30dea6e3
Essence Alexander <upperlefty@zoho.eu>
2018-06-20 23:05:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function bubbleSort(arr){
      for(var j = 1; j<=i; j++){
        if(arr[j-1]>arr[j]){
            var temp = arr[j-1];
+          arr[j-1] = arr[j];
+           arr[j] = temp;
+        }
+     }
+   }
+   return arr;
+}
+bubbleSort([7,5,2,4,3,9]); //[2, 3, 4, 5, 7, 9]
+bubbleSort([9,7,5,4,3,1]); //[1, 3, 4, 5, 7, 9]
+bubbleSort([1,2,3,4,5,6]); //[1, 2, 3, 4, 5, 6]
