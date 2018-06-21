d82c9215466b545ca3559c49270dc1c58e3e6046
Essence Alexander <upperlefty@zoho.eu>
2018-06-20 23:04:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function bubbleSort(arr){
+   var len = arr.length;
+  for (var i = len-1; i>=0; i--){
+     for(var j = 1; j<=i; j++){
+       if(arr[j-1]>arr[j]){
+           var temp = arr[j-1];
